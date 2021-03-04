15e2f6663910233e3d388b148dbd975822939acb
RVelhinho <ricardo_velhinho96@hotmail.com>
2021-03-04 19:09:34 +0000

    no data found placeholder

--- front-end/src/pages/main-page/main-page.jsx
+++ front-end/src/pages/main-page/main-page.jsx
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import L from 'leaflet';
 import * as turf from '@turf/turf';
 import worldPoly from '../../utils/world-poly.json';
+import dataIcon from '../../assets/images/data.svg';
 import CalendarContainer from '../../components/calendar-container/calendar-container';
 import MapContainer from '../../components/map-container/map-container';
 import DensityPlotContainer from '../../components/density-plot-container/density-plot-container';
@@ -617,57 +618,119 @@ export default class MainPage extends Component {
 								: 'col-4 px-0 main-page-container__left-section'
 						}
 					>
+						{calendarData.data.length === 0 && (
+							<div className='main-page-container__no-data-container'>
+								<img
+									src={dataIcon}
+									alt='no data found'
+									className='main-page-container__no-data-container__image'
+								/>
+								<span className='main-page-container__no-data-container__text'>
+									No Data Found
+								</span>
+							</div>
+						)}
 						<div className='row mx-0 main-page-container__left-section__top-row'>
 							<div className='col px-0 h-100'>
-								<CalendarContainer
-									data={calendarData.data}
-									hopLegendColors={calendarData.hopLegendColors}
-									tooltipType={calendarData.tooltipType}
-									onClickCalendarCircle={this.handleClickCircle}
-									onClickContinentCircle={this.handleClickContinentCircle}
-									onClickYear={this.handleClickYear}
-								/>
+								{calendarData.data.length !== 0 && (
+									<CalendarContainer
+										data={calendarData.data}
+										hopLegendColors={calendarData.hopLegendColors}
+										tooltipType={calendarData.tooltipType}
+										onClickCalendarCircle={this.handleClickCircle}
+										onClickContinentCircle={this.handleClickContinentCircle}
+										onClickYear={this.handleClickYear}
+									/>
+								)}
 							</div>
 						</div>
 						<div className='row mx-0 main-page-container__left-section__bottom-row'>
 							<div className='col px-0'>
-								<DensityPlotContainer
-									data={densityPlotData.data}
-									dateGrouper={densityPlotData.dateGrouper}
-									categoryAxis={densityPlotData.categoryAxis}
-									timeAttr={densityPlotData.timeAttr}
-									lineType={densityPlotData.lineType}
-									dataKey={densityPlotData.dataKey}
-									color={densityPlotData.color}
-									tooltipType={densityPlotData.tooltipType}
-									minValue={minValue}
-									maxValue={maxValue}
-								/>
+								{densityPlotData.data.length !== 0 && (
+									<DensityPlotContainer
+										data={densityPlotData.data}
+										dateGrouper={densityPlotData.dateGrouper}
+										categoryAxis={densityPlotData.categoryAxis}
+										timeAttr={densityPlotData.timeAttr}
+										lineType={densityPlotData.lineType}
+										dataKey={densityPlotData.dataKey}
+										color={densityPlotData.color}
+										tooltipType={densityPlotData.tooltipType}
+										minValue={minValue}
+										maxValue={maxValue}
+									/>
+								)}
 							</div>
 						</div>
 					</div>
 					<div className='col px-0'>
 						<div className='row mx-0 main-page-container__right-section__top-row'>
 							<div className='col px-0'>
-								<MapContainer
-									data={mapData.data}
-									tooltipType={mapData.tooltipType}
-									randomPositions={randomPositions}
-									onClickMapCircle={this.handleClickCircle}
-									onClickMapRing={this.handleClickMapRing}
-									onClickTimeRing={this.handleClickTimeRing}
-									onZoomOut={this.handleZoomOut}
-								/>
+								{mapData.data.length === 0 && (
+									<div className='main-page-container__no-data-container'>
+										<img
+											src={dataIcon}
+											alt='no data found'
+											className='main-page-container__no-data-container__image'
+										/>
+										<span className='main-page-container__no-data-container__text'>
+											No Data Found
+										</span>
+									</div>
+								)}
+								{mapData.data.length !== 0 && (
+									<MapContainer
+										data={mapData.data}
+										tooltipType={mapData.tooltipType}
+										randomPositions={randomPositions}
+										onClickMapCircle={this.handleClickCircle}
+										onClickMapRing={this.handleClickMapRing}
+										onClickTimeRing={this.handleClickTimeRing}
+										onZoomOut={this.handleZoomOut}
+									/>
+								)}
 							</div>
 						</div>
 						<div className='row mx-0 main-page-container__right-section__bottom-row'>
-							<div className='col-4 h-100 p-4 main-page-container__right-section__bottom-row__chart'>
-								<DonutChartContainer
-									data={donutChartData.data}
-									tooltipType={donutChartData.tooltipType}
-								/>
+							<div
+								className={
+									donutChartData.data.length === 0
+										? 'col-4 h-100 px-0 main-page-container__right-section__bottom-row__chart'
+										: 'col-4 h-100 p-4 main-page-container__right-section__bottom-row__chart'
+								}
+							>
+								{donutChartData.data.length === 0 && (
+									<div className='main-page-container__no-data-container'>
+										<img
+											src={dataIcon}
+											alt='no data found'
+											className='main-page-container__no-data-container__image'
+										/>
+										<span className='main-page-container__no-data-container__text'>
+											No Data Found
+										</span>
+									</div>
+								)}
+								{donutChartData.data.length !== 0 && (
+									<DonutChartContainer
+										data={donutChartData.data}
+										tooltipType={donutChartData.tooltipType}
+									/>
+								)}
 							</div>
 							<div className='col-4 h-100 px-0 main-page-container__right-section__bottom-row__chart'>
+								{wordCloudData.data.length === 0 && (
+									<div className='main-page-container__no-data-container'>
+										<img
+											src={dataIcon}
+											alt='no data found'
+											className='main-page-container__no-data-container__image'
+										/>
+										<span className='main-page-container__no-data-container__text'>
+											No Data Found
+										</span>
+									</div>
+								)}
 								{wordCloudData.data.length !== 0 && (
 									<WordCloudContainer
 										data={wordCloudData.data}
@@ -680,12 +743,26 @@ export default class MainPage extends Component {
 								)}
 							</div>
 							<div className='col-4 h-100 px-0 main-page-container__right-section__bottom-row__chart'>
-								<BarChartContainer
-									data={barChartData.data}
-									gradientColors={barChartData.gradientColors}
-									tooltipType={barChartData.tooltipType}
-									onClickBar={this.handleClickBar}
-								/>
+								{barChartData.data.length === 0 && (
+									<div className='main-page-container__no-data-container'>
+										<img
+											src={dataIcon}
+											alt='no data found'
+											className='main-page-container__no-data-container__image'
+										/>
+										<span className='main-page-container__no-data-container__text'>
+											No Data Found
+										</span>
+									</div>
+								)}
+								{barChartData.data.length !== 0 && (
+									<BarChartContainer
+										data={barChartData.data}
+										gradientColors={barChartData.gradientColors}
+										tooltipType={barChartData.tooltipType}
+										onClickBar={this.handleClickBar}
+									/>
+								)}
 							</div>
 						</div>
 					</div>
